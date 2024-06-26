ef30dd99ed317270734291fc56070aa648d69f24
TieOnly <100069442+TieOnly@users.noreply.github.com>
2024-06-26 10:28:27 +0700

    #12-proj
    Update source

---
+++ #12_SmartClock_STM32F1_TFTDisplay/Demo/Core/Src/main.c
@@ -0,0 +1,14 @@
+#include "sys.h"
+
+
+
+int main(void)
+{
+	user_t user;
+	user_inst(&user, "tieonly");
+	user.init(&user);
+	// while (1) { user.loop(&user); }
+	vTaskStartScheduler();
+	for( ;; );
+	return 0;
+}
